7446ed315e1cab0c6f11787db231c8dafe54ac4d
Noisk8 <juansk8jaramillo@gmail.com>
2023-11-16 01:00:40 -0500

    Update landing.jsx

--- landing.jsx
+++ landing.jsx
@@ -53,95 +53,184 @@ const fontweight = props.fontweight;
 
 return (
   <>
-    <div class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
+    <div className="bg-light-subtle">
+      <div class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <Web3Connect
+            className="ConnectButton "
+            connectLabel="Connect with Web3"
+          />
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse btn-group "
+            role="group"
+            id="navbarSupportedContent"
+            aria-label="Basic outlined example"
+          >
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  <button type="button" class="btn btn-outline-primary">
+                    Acerca De
+                  </button>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <button type="button" class="btn btn-outline-primary">
+                    Docomentación
+                  </button>
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <button type="button" class="btn btn-outline-primary">
+                    Github
+                  </button>
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <button type="button" class="btn btn-outline-primary">
+                        Middle
+                      </button>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <button type="button" class="btn btn-outline-primary">
+                        Left
+                      </button>
+                    </a>
+                  </li>
 
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">
-                Disabled
-              </a>
-            </li>
-          </ul>
-          <div class="d-flex" role="search">
-            <Web3Connect
-              className="ConnectButton "
-              connectLabel="Connect with Web3"
-            />
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <button type="button" class="btn btn-outline-primary">
+                        Middle
+                      </button>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <div class="d-flex" role="search"></div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="container text-center mt-5">
-      <div className="d-flex flex-column align-items-center mb-3">
-        <img
-          src="https://raw.githubusercontent.com/Noisk8/Componente-BOS/main/giveme.png"
-          alt="Gif"
-          width="300px"
-          className="mb-4"
-        />
+      <div className="container text-center mt-5">
+        <div className="d-flex flex-column align-items-center mb-3">
+          <h1> Tipe Me </h1>
+          <img
+            src="https://raw.githubusercontent.com/Noisk8/Componente-BOS/main/tipeme.png"
+            alt="Gif"
+            width="200px"
+            className="mb-4"
+          />
 
-        <Button0018
-          fontsize={fontsize}
-          fontweight={fontweight}
-          onClick={giveme}
-        >
-          <span>Give me </span>
-        </Button0018>
+          <div className="d-flex justify-content-center align-items-center flex-column bg-light p-3 shadow-lg rounded">
+            <div className="form-group">
+              <input
+                className="form-control mt-2"
+                type="text"
+                id="name"
+                name="name"
+                required
+                minLength="4"
+                maxLength="8"
+                size="10"
+                placeholder="Nombre del Donante"
+              />
+            </div>
+
+            <div className="form-group">
+              <textarea
+                class="form-control mt-4"
+                placeholder="Sugerencias"
+                aria-label="With textarea"
+              ></textarea>
+            </div>
+
+            <div className="form-group mt-4 mb-4">
+              <select
+                class="form-select"
+                id="floatingSelect"
+                aria-label="Floating label select example"
+              >
+                <option selected>Calificación</option>
+                <option value="1">⭐</option>
+                <option value="2">⭐⭐</option>
+                <option value="3">⭐⭐⭐</option>
+                <option value="3">⭐⭐⭐⭐</option>
+                <option value="3">⭐⭐⭐⭐⭐</option>
+              </select>
+            </div>
+            <Button0018
+              fontsize={fontsize}
+              fontweight={fontweight}
+              onClick={giveme}
+            >
+              <span>Give me </span>
+            </Button0018>
+          </div>
+
+          <h1 className="mt-4 mb-4 text-center"> Ultimas Donaciones</h1>
+          <table className="table table-bordered table-hover table-responsive bg-light p-3 shadow-lg rounded">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">Fecha</th>
+                <th scope="col">Articulo</th>
+                <th scope="col">Donante</th>
+                <th scope="col">Mensaje</th>
+                <th scope="col">Rating</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>New Data 1</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+
+                <td>New Data 3</td>
+              </tr>
+              <tr className="table-info">
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td> dato 3</td>
+                <td>@twitter</td>
+                <td>New Data 5</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="card-footer bg-transparent border-success">
+          Tipe Me 2023
+        </div>
       </div>
     </div>
   </>
